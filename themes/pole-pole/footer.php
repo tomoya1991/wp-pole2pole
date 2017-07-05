433827e5813ea480ae9b63e362e6b8491a11dd12
tomoya.ito <tomoya.ito.1991@gmail.com>
2017-07-05 19:25:13 +0900

    githubのテスト。コミットできるかな。
    コミットできるかなあー。

--- themes/pole-pole/footer.php
+++ themes/pole-pole/footer.php
@@ -1,10 +1,6 @@
 </div><!-- #wrapper -->
 
 <footer>
-    <div id="footer_img">
-	<img src="<?php bloginfo('template_url'); ?>/assets/img/footer_log.jpg">
-    <h2>Cpyright(c) pole-pole texitile labo All Rights Reserved.</h2>
-        </div>
 </footer>
 
 <script src="<?php bloginfo('template_url'); ?>/assets/js/lib/jquery-1.11.3.min.js"></script>
