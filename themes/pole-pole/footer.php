c360c9357cc6f9533ef60e38d25a22070543e491
tomoya.ito <tomoya.ito.1991@gmail.com>
2017-07-05 19:26:07 +0900

    ギットハブのテスト２
    コミットして下さい。

--- themes/pole-pole/footer.php
+++ themes/pole-pole/footer.php
@@ -1,10 +1,14 @@
 </div><!-- #wrapper -->
 
 <footer>
+  <div id="footer_img">
+  	<img src="<?php bloginfo('template_url'); ?>/assets/img/footer_log.jpg">
+      <h2>Cpyright(c) pole-pole texitile labo All Rights Reserved.</h2>
+  </div>
 </footer>
-
 <script src="<?php bloginfo('template_url'); ?>/assets/js/lib/jquery-1.11.3.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/main.js"></script>
 
 </body>
 </html>
+<!-- gitub desktop テストです-->
